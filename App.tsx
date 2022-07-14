9b3dbb8129394dda1c3cdc5222127b7dbc835b14
Anton Yanovskyi <anton_yanovskyi@epam.com>
2022-07-14 23:51:42 +0300

    Base list view

--- App.tsx
+++ App.tsx
@@ -1,20 +1,117 @@
+import React, { useState } from "react";
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {FlatList, SafeAreaView, StyleSheet, Text, View, Linking, Pressable, Alert, Modal} from 'react-native';
+
+const CURRENCY = 'UAH';
+
+const wishlistData = [
+  {
+    name: 'T-shirt',
+    url: 'https://7mntns.com/man/t-shirt/4362/',
+    price: '440'
+  },
+  {
+    name: 'Book',
+    url: 'https://7mntns.com/man/t-shirt/4362/',
+    price: '120'
+  },
+  {
+    name: 'Refrigerator',
+    url: 'https://7mntns.com/man/t-shirt/4362/',
+    price: '12000'
+  }
+];
+
+const Item = ({ name, price, url }) => (
+    <View style={styles.item}>
+      <Pressable onPress={() => Linking.openURL(url)}>
+        <Text style={styles.name}>{name}</Text>
+        <Text>{price} {CURRENCY}</Text>
+      </Pressable>
+    </View>
+);
 
 export default function App() {
+  const [modalVisible, setModalVisible] = useState(false);
+  const renderItem = ({ item }) => (
+      <Item name={item.name} price={item.price} url={item.url}/>
+  );
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <Text>My Wishlist</Text>
+        <Pressable onPress={() => setModalVisible(!modalVisible)}>
+          <View style={styles.addButton}>
+            <Text style={styles.addButtonText}>I wish +</Text>
+          </View>
+        </Pressable>
+      </View>
+      <View style={styles.listContainer}>
+        <FlatList data={wishlistData} renderItem={renderItem}/>
+      </View>
       <StatusBar style="auto" />
-    </View>
+      <View style={styles.centeredView}>
+        <Modal
+            animationType="slide"
+            transparent={false}
+            visible={modalVisible}
+            onRequestClose={() => {
+              Alert.alert("Modal has been closed.");
+              setModalVisible(!modalVisible);
+            }}
+        >
+          <View style={styles.centeredView}>
+            <Text>Modal content</Text>
+            <Pressable
+                onPress={() => setModalVisible(!modalVisible)}
+            >
+              <Text>Hide Modal</Text>
+            </Pressable>
+          </View>
+        </Modal>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
+  listContainer: {
+    flex: 1
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  name: {
+    fontSize: 32,
+  },
+  header: {
+    flexDirection: 'row',
+    paddingHorizontal: 20,
+    alignItems: "center",
+    justifyContent: "space-between"
+  },
+  addButton: {
+    backgroundColor: '#f00',
+    borderRadius: 10,
+    paddingHorizontal: 10,
+    paddingVertical: 5
+  },
+  addButtonText: {
+    color: '#fff',
+    fontWeight: "700"
+  }
 });
